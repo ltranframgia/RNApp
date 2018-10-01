b6d39a17d504ed8e788e22fed27bb6acbcaf1f02
Henry Tran <ltranframgia@gmail.com>
2018-10-01 16:29:52 +0700

    config for react native debuger

--- src/store/store.js
+++ src/store/store.js
@@ -4,10 +4,17 @@ import thunkMiddleware from 'redux-thunk'
 // import { createLogger } from 'redux-logger'
 
 // Store
-const store = createStore(reducers, applyMiddleware(
-  thunkMiddleware
-  // loggerMiddleware 
-));
+const configureStore = (reducers) => {
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const enhancer = composeEnhancers(
+    applyMiddleware(
+      thunkMiddleware
+    ),
+  );
+  return createStore(reducers, enhancer);
+};
+
+const store = configureStore(reducers)
 
 const dispatchAction = (action) => {
   store.dispatch(action)
